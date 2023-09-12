0b6180fdbf3cf27a8331c883c07ea9316481e5f8
WhiteFang <anjugyanprakash@gmail.com>
2023-09-12 21:33:37 +0530

    added next_day program

--- Lab 2 (11th Sep 23)/next_day.cpp
+++ Lab 2 (11th Sep 23)/next_day.cpp
@@ -5,35 +5,53 @@ int main()
 	int y, m, d;
 	
 	//Taking input from the user
-	printf("Enter the year, month and date respectively... \n");
-	scanf("%d", &y);
-	scanf("%d", &m);
+	printf("Enter the date, month and year respectively... \n");
 	scanf("%d", &d);
+	scanf("%d", &m);
+	scanf("%d", &y);
 	
-	if( d == 30 || d == 31 || d == 28 || d == 29)
+	if( d == 30 || d == 31 || d == 28 || d == 29)  //Checking for ending dates
 	{
-		if(m!=2)
+		if(d==30 &&(m == 4 || m == 6 || m == 9 || m == 11)) //Checking for months with 30 days
 		{
-			if(m == 12)
-			{
-				printf("%d / %d / %d",1,1,y+1);
-			}
-			else{
-				printf("%d / %d / %d",1,m+1,y);
-			}
+			printf("%d / %d / %d",1,m+1,y);
 		}
-		else if(m == 2)
+		else if(d==31 &&(m == 1 ||  m == 3|| m == 5 || m == 7 || m == 8 || m == 10)) //Checking for months with 31 days
 		{
-			if(year%4==0 &&(year%100 !=0 || year%400 ==0))
+			printf("%d / %d / %d",1,m+1,y);
+		}
+		else if(d == 31 && m == 12) //Checking for December separately
+		{
+			printf("%d / %d / %d",1,1,y+1);
+		}
+		else if(m == 2) //Checking for Feburary
+		{
+			if(y%4==0 &&(y%100 !=0 || y%400 ==0)) //Checking for Leap Year
 	        {
-		      	printf("The year %d is a leap year.\n", year);
+		      	if(d == 28)
+				{
+					printf("%d / %d / %d", d+1,m,y);
+				}
+				else if(d == 29)
+				{
+					printf("%d / %d / %d", 1,m+1,y);
+				}
 	  		}
 			else
 			{
-				printf("The year %d is not a leap year.\n", year);
+				if(d == 28)
+				{
+					printf("%d / %d / %d", 1,m+1,y);
+				}
 			}
-			printf("%d / %d / %d",1,m+1,y);
 		}
+		else //Nothing Matched then just increasing the day by 1
+		{
+			printf("%d / %d / %d",d+1,m,y);
+		}
+	}
+	else{
+		printf("%d / %d / %d",d+1,m,y);
 	}
 	
 }
